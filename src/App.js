264c46a2ee2371cb132c1c21fb380671aa79edc1
mergeflydemo <54778942+mergeflydemo@users.noreply.github.com>
2020-03-05 23:36:31 -0500

    demoPR
    add counter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/Welcome';
+import Counter from './components/Counter';
 
 function App() {
   return (
@@ -8,16 +10,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my demo React App!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Welcome />
+        <p>
+          <Counter />
+        </p>
       </header>
     </div>
   );
